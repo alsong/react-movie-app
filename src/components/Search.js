5f0178fb7f7cfd17363561546034db9e6c2ea052
alsong <alsongdunstan2@gmail.com>
2022-09-19 14:03:34 +0300

    initial commit

---
+++ src/components/Search.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import { TextField } from "@mui/material";
+import { useSelector, useDispatch } from "react-redux";
+import useStyle from "../styles";
+import { getMovies } from "../redux/feature/movieSlice";
+
+const Search = () => {
+  const [name, setName] = useState("spider");
+  const classes = useStyle();
+  const {
+    moviesList: { Error: error },
+  } = useSelector((state) => ({ ...state.movie }));
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getMovies(name));
+  }, [name, dispatch]);
+
+  return (
+    <>
+      <h2 className={classes.title}>Movie Search App</h2>
+      <form className={classes.form} onSubmit={(e) => e.preventDefault()}>
+        <TextField
+          type="text"
+          fullWidth
+          value={name}
+          sx={{ m: 1, width: "55ch" }}
+          onChange={(e) => setName(e.target.value)}
+        />
+        {error && <p className={classes.error}>{error}</p>}
+      </form>
+    </>
+  );
+};
+
+export default Search;
